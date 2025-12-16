d981635830421214145f91b2cde01812e64a57ff
Jayalakshmi <etlakshmi@gmail.com>
2025-12-16 14:46:20 +0530

    Defined function code

---
+++ main.py
@@ -0,0 +1,10 @@
+def add(Number1,Number2):
+    return Number1+Number2
+def subtract(Number1,Number2):
+    return Number1-Number2
+def multiply(Number1,Number2):
+    return Number1*Number2
+def divide(Number1,Number2):
+    if Number2==0:
+        return "Error! Cannot divide by zero."
+    return Number1/Number2
